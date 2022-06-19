dd6f5c9b6ad88f8ac22f6f84f97e6647da05d1f5
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-06-19 23:22:51 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 11.1 .repo/local_manifests
-repo sync --no-tags --no-clone-bundle
+repo sync --no-tags --no-clone-bundle -j8
 
 # build rom
 source build/envsetup.sh
