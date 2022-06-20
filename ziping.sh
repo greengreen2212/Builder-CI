84814247b414ba0d5b11f9dbe59e3da07ed0f0f6
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-06-20 10:49:13 +0700

    Fix maybe

--- ziping.sh
+++ ziping.sh
@@ -8,13 +8,18 @@ function enviroment() {
    device=$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
    name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
    JOS=$WORKDIR/rom/$name_rom/out/target/product/$device/*.zip
+   SHASUM=$WORKDIR/rom/$name_rom/out/target/product/$device/*.zip*sha*
+   OTA=$WORKDIR/rom/$name_rom/out/target/product/$device/*ota*.zip
+
 }
 
 function upload_rom() {
    msg Upload rom..
-   curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="<b>Build status:</b>%0A@Bella_Aprilia_27 <code>Building Rom $name_rom succes [✔️]</code>"
+   rm -rf $SHASUM
+   rm -rf $OTA
+   curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="<b>Build status:</b>%0A@NiatIngsungLakenMalemJumat <code>Building Rom $name_rom succes [✔️]</code>"
    rclone copy --drive-chunk-size 256M --stats 1s $JOS NFS:rom/$name_rom -P
-   curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="Link : https://needforspeed.projek.workers.dev/rom/$name_rom/$(cd $WORKDIR/rom/$name_rom/out/target/product/$device && ls $name_rom*.zip)"
+   curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="Link : https://nfsproject.projek.workers.dev/0/$name_rom/$(cd $WORKDIR/rom/$name_rom/out/target/product/$device && ls $name_rom*.zip)"
    msg Upload rom succes..
 }
 
