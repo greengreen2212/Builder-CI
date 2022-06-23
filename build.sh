4ad1e22581b110b3db6ef820e46df56cb6445b0a
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-06-24 00:19:44 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 11.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
 # build rom
 source build/envsetup.sh
-lunch aicp_rosy-userdebug
+lunch havoc_rosy-user
+lunch havoc_rosy-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 export NFS_ADD_GCAMGO=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-brunch rosy #&
-#sleep 70m
-#kill %1
+brunch rosy &
+sleep 70m
+kill %1
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
